3d087263afcd5c2219d7d5a36925cafed02f893e
TapirLiu <9266865+TapirLiu@users.noreply.github.com>
2025-02-06 00:53:18 +0800

    support "tmd fmt" in wasm
    "tmd fmt" all docs

--- wasm/tmd.zig
+++ wasm/tmd.zig
@@ -31,11 +31,19 @@ export fn get_version() isize {
 }
 
 export fn tmd_to_html(fullHtmlPage: bool, supportCustomBlocks: bool) isize {
-    const htmlWithLengthHeader = generate(fullHtmlPage, supportCustomBlocks) catch |err| {
-        logMessage("generate error: ", @errorName(err), @intFromError(err));
+    const htmlWithLengthHeader = generateHTML(fullHtmlPage, supportCustomBlocks) catch |err| {
+        logMessage("generate HTML error: ", @errorName(err), @intFromError(err));
         return -@as(i32, @intFromError(err));
     };
     return @intCast(@intFromPtr(htmlWithLengthHeader.ptr));
+}
+
+export fn tmd_format() isize {
+    const tmdWithLengthHeader = formatTMD() catch |err| {
+        logMessage("format TMD error: ", @errorName(err), @intFromError(err));
+        return -@as(i32, @intFromError(err));
+    };
+    return @intCast(@intFromPtr(tmdWithLengthHeader.ptr));
 }
 
 fn init() ![]u8 {
@@ -61,27 +69,46 @@ fn writeWersion() ![]u8 {
     return buffer;
 }
 
-fn generate(fullHtmlPage: bool, supportCustomBlocks: bool) ![]u8 {
+const InputData = struct {
+    suffixForIdsAndNames: []const u8,
+    tmdData: []const u8,
+    freeBuffer: []u8,
+};
+
+fn retrieveInputData() !InputData {
     if (buffer.len == 0) {
         return error.BufferNotCreatedYet;
     }
 
     var fbs = std.io.fixedBufferStream(buffer);
-    const suffixForIdsAndNames: []const u8 = blk: {
+    const suffix: []const u8 = blk: {
         const suffixLen = try fbs.reader().readByte();
         break :blk buffer[1 .. 1 + suffixLen];
     };
-    fbs = std.io.fixedBufferStream(buffer[1 + suffixForIdsAndNames.len ..]);
+    fbs = std.io.fixedBufferStream(buffer[1 + suffix.len ..]);
     const tmdDataLength = try fbs.reader().readInt(u32, .little);
     if (tmdDataLength > maxInFileSize) {
         return error.DataSizeTooLarge;
     }
 
-    const tmdDataStart = 1 + 4 + suffixForIdsAndNames.len;
+    const tmdDataStart = 1 + 4 + suffix.len;
     const tmdDataEnd = tmdDataStart + tmdDataLength;
 
     const tmdContent = buffer[tmdDataStart..tmdDataEnd];
     const remainingBuffer = buffer[tmdDataEnd..];
+
+    return .{
+        .suffixForIdsAndNames = suffix,
+        .tmdData = tmdContent,
+        .freeBuffer = remainingBuffer,
+    };
+}
+
+fn generateHTML(fullHtmlPage: bool, supportCustomBlocks: bool) ![]u8 {
+    const inputData = try retrieveInputData();
+    const suffixForIdsAndNames = inputData.suffixForIdsAndNames;
+    const tmdContent = inputData.tmdData;
+    const remainingBuffer = inputData.freeBuffer;
 
     var fba = std.heap.FixedBufferAllocator.init(remainingBuffer);
     const fbaAllocator = fba.allocator();
@@ -107,7 +134,7 @@ fn generate(fullHtmlPage: bool, supportCustomBlocks: bool) ![]u8 {
     //logMessage("", "fba.end_index: ", @intCast(fba.end_index));
 
     const renderBuffer = try fbaAllocator.alloc(u8, remainingBuffer.len - fba.end_index);
-    fbs = std.io.fixedBufferStream(renderBuffer);
+    var fbs = std.io.fixedBufferStream(renderBuffer);
     try fbs.writer().writeInt(u32, 0, .little);
 
     try tmdDoc.toHTML(fbs.writer(), fullHtmlPage, supportCustomBlocks, suffixForIdsAndNames, std.heap.wasm_allocator);
@@ -118,4 +145,35 @@ fn generate(fullHtmlPage: bool, supportCustomBlocks: bool) ![]u8 {
     //logMessage("", "htmlWithLengthHeader.len: ", @intCast(htmlWithLengthHeader.len));
 
     return htmlWithLengthHeader;
+}
+
+fn formatTMD() ![]u8 {
+    const inputData = try retrieveInputData();
+    //const suffixForIdsAndNames = inputData.suffixForIdsAndNames;
+    const tmdContent = inputData.tmdData;
+    const remainingBuffer = inputData.freeBuffer;
+
+    var fba = std.heap.FixedBufferAllocator.init(remainingBuffer);
+    const fbaAllocator = fba.allocator();
+
+    // parse file
+
+    var tmdDoc = try tmd.Doc.parse(tmdContent, fbaAllocator);
+
+    // format file
+
+    const formatBuffer = try fbaAllocator.alloc(u8, remainingBuffer.len - fba.end_index);
+    var fbs = std.io.fixedBufferStream(formatBuffer);
+    try fbs.writer().writeInt(u32, 0, .little);
+
+    try tmdDoc.toTMD(fbs.writer(), true);
+
+    const tmdWithLengthHeader = fbs.getWritten();
+    try fbs.seekTo(0);
+    const length = if (std.mem.eql(u8, tmdContent, tmdWithLengthHeader[4..])) 0 else tmdWithLengthHeader.len - 4;
+    try fbs.writer().writeInt(u32, length, .little);
+
+    //logMessage("", "tmdWithLengthHeader.len: ", @intCast(tmdWithLengthHeader.len));
+
+    return tmdWithLengthHeader;
 }
